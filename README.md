e22297908d706a7c344563ea0f072689f0ae7720
Nguyen Hai Chau <nhchau@gmail.com>
2015-03-20 18:15:49 +0700

    Modified R code and modify README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # rpkg
+This R script and rpkg.csv are used for quick installation
+of my frequently used packages.
+
+To use the package, run the following command in R:
+```
+source('https://github.com/nhchauvnu/rpkg/inspkg.r')
+```
+to check installed packages:
+```
+list1()
+```
+to check not-installed packages:
+```
+list0()
+```
+
+To installed all frequently used packages:
+```
+insmypkg()
+```
+To install a single package:
+```
+inspkg('packagename')
+```
+To check packages status:
+```
+prinpkg()
+```
+To check status of a single package:
+```
+chkpkg('packagename')
+```
---
+++ inspkg.r
@@ -0,0 +1,50 @@
+pkglist = 'http://git.com/nhchauvnu/rpkg/rpkg.csv'
+chkpkg = function(pkg) {
+	pkglist = installed.packages()[,1]
+	m = sum(pkglist == pkg)
+	if (m <= 0) return(FALSE)
+	else return(TRUE)
+}
+
+printstat = function(pkg) {
+	if (chkpkg(pkg)) print(paste("Installed:",pkg))
+	else print(paste("Not installed:",pkg))
+}
+
+printpkg = function() {
+	pkg = as.character(read.csv(pkglist)$pkg)
+	for (i in pkg)
+		printstat(i)
+}
+
+list0 = function() {
+	pkg = as.character(read.csv(pkglist)$pkg)
+	print("Not yet installed packages:")
+	for (i in pkg)
+		if (!chkpkg(i)) print(i)
+}
+
+list1 = function() {
+	pkg = as.character(read.csv(pkglist)$pkg)
+	print("Not yet installed packages:")
+	for (i in pkg)
+		if (chkpkg(i)) print(i)
+}
+
+inspkg = function(pkg) {
+	if (!chkpkg(pkg)) {
+		print(paste(" >>> Installing ",pkg))
+		install.packages(pkg)
+	}
+	else print(paste(" >>> Package", pkg, "is already installed"))
+}
+
+insmypkg = function() {
+	pkg = as.character(read.csv(pkglist)$pkg)
+	for (i in pkg)
+		if (!chkpkg(i)) {
+			print(paste(" >>> Installing ",i))
+			install.packages(i)
+		}
+}
+
